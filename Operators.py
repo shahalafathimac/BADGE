e6a48fae55435299dcc53ba99ee3df585591b782
shahalafathima213-alt <shahalafathima213@gmail.com>
2026-04-11 17:15:16 +0530

    Add initial implementation of Operators.py with various examples

---
+++ Operators.py
@@ -0,0 +1,107 @@
+# a = [1,2]
+# b = a
+# print(a is b)
+
+
+# marks = 75
+# if marks>=90:
+#     print('A')
+# elif marks>=80:
+#     print("B")
+# elif marks>=70:
+#     print("c")
+# else:
+#     print("D")
+
+
+# day = 4
+# match day:
+#     case 1:
+#         print("monday")
+#     case 2:
+#         print("tuesday")
+#     case 3:
+#         print("wednesday")
+#     case 4:
+#         print("thursday")
+#     case _:
+#         print("invalid")
+
+# age = 20
+# if age >= 18:
+#     if age >= 60:
+#         print("Senior")
+#     else:
+#         print("Adult")
+
+# for i in range(1,5):
+#     print(i)
+
+
+
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)
+
+
+# for i in range(5):
+#     pass
+
+
+# for i in range(1,3):
+#     print(i)
+# else:
+#     print("done")
+
+# for i in range(2):
+#     for j in range(2):
+#         for k in range(2):
+#             print(i, j, k)
+
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+
+
+# i = 1
+
+# while True:
+#     print(i)
+#     i += 1
+
+#     if i > 5:
+#         break
+
+
+# name = input("eneter your name:")
+# print(name)
+
+# age = int(input("eneter your age:"))
+# print(age+5)
+
+# print("hello","name", end="-")
+# print("world")
+
+# print("A","B","C", sep=".")
+
+# print("hello\nworld")
+
+# for i in range(5,11,2):
+#     print(i)
+
+
+# for i in range(5,1,-1):
+#     print(i)
+
+# names = ["A","B","C"]
+
+# for i, name in enumerate(names):
+#     print(i, name)
+
+# names = ["A","B","C"]
+
+# for i, name in enumerate(names, start=1):
+#     print(i, name)
+
